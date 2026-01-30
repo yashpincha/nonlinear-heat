e48501a95c5502bbb6fab141c39bba6c7dc274eb
Yash Pincha <yashpincha0706@gmail.com>
2026-01-30 22:44:29 +0000

    building models with pymc

--- models/models.py
+++ models/models.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pymc as pm
 from base import BaseModel
 
 class SimpleForward(BaseModel):
@@ -7,14 +8,30 @@ class SimpleForward(BaseModel):
     assumes high-frequency + heavily-damped limit where reflections are negligible.
     T(x,t) = Tbar + A * exp(-beta*x) * cos(omega*t - beta*x + phi)
     '''
-    def forward(self, x, t):
-        D = self.params['D']
-        w = self.params['w']
-        A = self.params['A']
-        phi = self.params['phi']
-        Tbar = self.params['Tbar']
+    def forward(self, x, t, params):
+        D = params['D']
+        w = params['w']
+        A = params['A']
+        phi = params['phi']
+        Tbar = params['Tbar']
         beta = np.sqrt(w / (2 * D))
         return Tbar + A * np.exp(-beta * x) * np.cos(w * t - beta * x + phi)
+    
+    def build_model(self, x, t, data, sigma=None):
+        with pm.Model() as model:
+            D = pm.Uniform("D", lower=0.001, upper=0.1)
+            w = pm.Uniform("w", lower=0.01, upper=10.0)
+            A = pm.Uniform("A", lower=0.0, upper=10.0)
+            phi = pm.Uniform("phi", lower=-np.pi, upper=np.pi)
+            Tbar = pm.Uniform("Tbar", lower=0.0, upper=50.0)
+            
+            mu = self.forward(x, t, {'D': D, 'w': w, 'A': A, 'phi': phi, 'Tbar': Tbar})
+            
+            if sigma is None:
+                sigma = pm.HalfNormal("sigma", sigma=1.0)            
+            pm.Normal("obs", mu=mu, sigma=sigma, observed=data)
+        return model
+
 
 class FullSolution(BaseModel):
     '''
@@ -22,16 +39,34 @@ class FullSolution(BaseModel):
     T(x,t) = Tbar + Re[A * exp(i(wt+phi)) * cosh(k(L-x)) / cosh(kL)]
     where k = sqrt(i*w / D)
     '''
-    def forward(self, x, t):
-        D = self.params['D']
-        w = self.params['w']
-        A = self.params['A']
-        phi = self.params['phi']
-        L = self.params['L']
-        Tbar = self.params['Tbar']
+    def forward(self, x, t, params):
+        D = params['D']
+        w = params['w']
+        A = params['A']
+        phi = params['phi']
+        L = params['L']
+        Tbar = params['Tbar']
         k = np.sqrt((1j * w) / D)
         thermal_wave = A * np.exp(1j * (w * t + phi)) * (np.cosh(k * (L - x)) / np.cosh(k * L))
         return Tbar + np.real(thermal_wave)
+    
+    def build_model(self, x, t, data, sigma=None):
+        with pm.Model() as model:
+            D = pm.Uniform("D", 0.001, 0.1)
+            w = pm.Uniform("w", 0.01, 10.0)
+            A = pm.Uniform("A", 0.0, 10.0)
+            phi = pm.Uniform("phi", -np.pi, np.pi)
+            L = pm.Uniform("L", 0.01, 1.0)
+            Tbar = pm.Uniform("Tbar", 0.0, 50.0)
+            
+            mu = self.forward(x, t, {'D': D, 'w': w, 'A': A, 'phi': phi, 'L': L, 'Tbar': Tbar})
+            
+            if sigma is None:
+                sigma = pm.HalfNormal("sigma", sigma=1.0)
+            
+            pm.Normal("obs", mu=mu, sigma=sigma, observed=data)
+        return model
+
 
 class FullSolutionNewton(BaseModel):
     '''
@@ -40,17 +75,36 @@ class FullSolutionNewton(BaseModel):
     T(x,t) = Tbar + Re[A*exp(i(wt+phi)) * cosh(k(L-x))/cosh(kL)]
     where k = sqrt((gamma + i*w)/D)
     '''
-    def forward(self, x, t):
-        D = self.params['D']
-        w = self.params['w']
-        A = self.params['A']
-        phi = self.params['phi']
-        L = self.params['L']
-        gamma = self.params['gamma']
-        Tbar = self.params['Tbar']
+    def forward(self, x, t, params):
+        D = params['D']
+        w = params['w']
+        A = params['A']
+        phi = params['phi']
+        L = params['L']
+        gamma = params['gamma']
+        Tbar = params['Tbar']
         k = np.sqrt((gamma + 1j * w) / D)
         thermal_wave = A * np.exp(1j * (w * t + phi)) * (np.cosh(k * (L - x)) / np.cosh(k * L))
         return Tbar + np.real(thermal_wave)
+    
+    def build_model(self, x, t, data, sigma=None):
+        with pm.Model() as model:
+            D = pm.Uniform("D", 0.001, 0.1)
+            w = pm.Uniform("w", 0.01, 10.0)
+            A = pm.Uniform("A", 0.0, 10.0)
+            phi = pm.Uniform("phi", -np.pi, np.pi)
+            L = pm.Uniform("L", 0.01, 1.0)
+            gamma = pm.Uniform("gamma", 0.0, 1.0)
+            Tbar = pm.Uniform("Tbar", 0.0, 50.0)
+            
+            mu = self.forward(x, t, {'D': D, 'w': w, 'A': A, 'phi': phi, 'L': L, 'gamma': gamma, 'Tbar': Tbar})
+            
+            if sigma is None:
+                sigma = pm.HalfNormal("sigma", sigma=1.0)
+            
+            pm.Normal("obs", mu=mu, sigma=sigma, observed=data)
+        return model
+
 
 class FullSolutionRobin(BaseModel):
     '''
@@ -58,17 +112,36 @@ class FullSolutionRobin(BaseModel):
     accounts for convective loss at the tip: -dT/dx = H(T-Tbar)
     where H = h/k_th
     '''
-    def forward(self, x, t):
-        D = self.params['D']
-        w = self.params['w']
-        A = self.params['A']
-        phi = self.params['phi']
-        L = self.params['L']
-        gamma = self.params['gamma']
-        H = self.params['H']
-        Tbar = self.params['Tbar']
+    def forward(self, x, t, params):
+        D = params['D']
+        w = params['w']
+        A = params['A']
+        phi = params['phi']
+        L = params['L']
+        gamma = params['gamma']
+        H = params['H']
+        Tbar = params['Tbar']
         k = np.sqrt((gamma + 1j * w) / D)
         num = k * np.cosh(k * (L - x)) + H * np.sinh(k * (L - x))
         den = k * np.cosh(k * L) + H * np.sinh(k * L)        
         thermal_wave = A * np.exp(1j * (w * t + phi)) * (num / den)
         return Tbar + np.real(thermal_wave)
+    
+    def build_model(self, x, t, data, sigma=None):
+        with pm.Model() as model:
+            D = pm.Uniform("D", 0.001, 0.1)
+            w = pm.Uniform("w", 0.01, 10.0)
+            A = pm.Uniform("A", 0.0, 10.0)
+            phi = pm.Uniform("phi", -np.pi, np.pi)
+            L = pm.Uniform("L", 0.01, 1.0)
+            gamma = pm.Uniform("gamma", 0.0, 1.0)
+            H = pm.Uniform("H", 0.0, 10.0)
+            Tbar = pm.Uniform("Tbar", 0.0, 50.0)
+            
+            mu = self.forward(x, t, {'D': D, 'w': w, 'A': A, 'phi': phi, 'L': L, 'gamma': gamma, 'H': H, 'Tbar': Tbar})
+            
+            if sigma is None:
+                sigma = pm.HalfNormal("sigma", sigma=1.0)
+            
+            pm.Normal("obs", mu=mu, sigma=sigma, observed=data)
+        return model
